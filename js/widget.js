0920ea27bd13800da62fe6271a6891cbeeceb079
christophermayfield <christopher.j.mayfield@gmail.com>
2016-03-24 19:46:44 -0700

    Add final project

---
+++ js/widget.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var url = '../data/employees.json';
+  $.getJSON(url, function (response) {
+    
+    var statusHTML = '<ul class="bulleted">';
+    $.each(response, function(index, employee) {
+      if(employee.inoffice === true) {
+        statusHTML +='<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      
+      statusHTML += employee.name + '</li>';
+    }); //end loop
+    
+    statusHTML += '</ul>';
+    $('#employeeList').html(statusHTML);
+  }); //end JSON
+}); //end ready
